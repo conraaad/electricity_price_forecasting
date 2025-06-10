0907fc63fa34c2d4aa56eae7794cb0c3a47043b0
Conrad <49191555+conraaad@users.noreply.github.com>
2025-06-10 16:46:59 +0200

    merged gas data

--- model/src/merge.py
+++ model/src/merge.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
 
+export_path = '../data/results/def_gas.csv'
 # Load both CSV files
-dataset_1_df = pd.read_csv('../data/results/interchange_data.csv')
-dataset_2_df = pd.read_csv('../data/results/demand_data.csv')
+dataset_1_df = pd.read_csv('../data/results/gas_data.csv')
+dataset_2_df = pd.read_csv('../data/results/gas_price.csv')
 
 # Make sure datetime columns are in the same format
 dataset_1_df['datetime_iso'] = pd.to_datetime(dataset_1_df['datetime_iso'])
@@ -11,8 +12,8 @@ dataset_2_df['datetime_iso'] = pd.to_datetime(dataset_2_df['datetime_iso'])
 
 # Merge the dataframes on datetime columns
 merged_df = pd.merge(
-    dataset_2_df,
-    dataset_1_df[['datetime_iso', 'interchange_balance']],  # Select both datetime and value columns
+    dataset_1_df,
+    dataset_2_df,  #! Canviar aixo
     on='datetime_iso',  # Join on the datetime column
     how='left'  # Keep all rows from dataset_2_df
 )
@@ -23,5 +24,5 @@ print(f"Columns: {merged_df.columns.tolist()}")
 print(merged_df.head())
 
 # Save the result to a new CSV file
-merged_df.to_csv('../data/results/def_dataset.csv', index=False)
+merged_df.to_csv(export_path, index=False)
 
