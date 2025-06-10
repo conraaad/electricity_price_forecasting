51859235e357bab70f7e59a5e03dc20955ae9c7a
Conrad <49191555+conraaad@users.noreply.github.com>
2025-06-11 00:24:25 +0200

    residual_demand,solar_share_demand,wind_share_demand

--- model/src/solar.py
+++ model/src/solar.py
@@ -2,38 +2,47 @@
 import json
 import pandas as pd
 
-csv_path = '../data/results/solar_data.csv'
+csv_path = '../data/results/def_data/merged_def_dataset_solar.csv'
 
-with open('../data/source/generacio_gas.json', 'r', encoding='utf-8') as file:
-  solar_json = json.load(file)
+# with open('../data/source/generacio_gas.json', 'r', encoding='utf-8') as file:
+#   solar_json = json.load(file)
 
-solar_df = pd.read_csv(csv_path)
+df = pd.read_csv(csv_path)
+
+# df['solar'] = round(df['solar'], 4)
+# df['wind'] = round(df['wind'], 4)
+
+# df['residual_demand'] = round(df['demand'] - (df['solar'] + df['wind']), 4)
+
+df['solar_share_demand'] = round(df['solar'] / df['demand'], 4)
+df['wind_share_demand'] = round(df['wind'] / df['demand'], 4)
+
 
 
 # Extract interchange data from the JSON
 
 # datetimes = []
-gen_value = []
-for entry in solar_json['indicator']['values']:
-    if 'datetime_utc' in entry and 'value' in entry:
-        # datetimes.append(entry['datetime_utc'])
-        gen_value.append(entry['value'])
+# gen_value = []
+# for entry in solar_json['indicator']['values']:
+#     if 'datetime_utc' in entry and 'value' in entry:
+#         # datetimes.append(entry['datetime_utc'])
+#         gen_value.append(entry['value'])
 
-# solar_df = pd.DataFrame({
+# df = pd.DataFrame({
 #     'datetime_iso': datetimes,
 #     'solar': gen_value
 # })
 
-solar_df['gas_generation'] = gen_value
+# df['gas_generation'] = gen_value
 
-# solar_df = solar_df.rename(columns={'interchange_balance': 'solar'})
+# df = df.rename(columns={'interchange_balance': 'solar'})
 
-# solar_df['datetime_iso'] = pd.to_datetime(solar_df['datetime_iso'])
-# solar_df['datetime_iso'] = solar_df['datetime_iso'].dt.strftime('%Y-%m-%d %H:%M:%S')
+# df['datetime_iso'] = pd.to_datetime(df['datetime_iso'])
+# df['datetime_iso'] = df['datetime_iso'].dt.strftime('%Y-%m-%d %H:%M:%S')
 
 
 # Save the updated DataFrame back to CSV if needed
-solar_df.to_csv(csv_path, index=False)
+df.to_csv(csv_path, index=False)
 print(f"Updated DataFrame saved to {csv_path}")
 
-print(solar_df.head())
+print(df.head())
