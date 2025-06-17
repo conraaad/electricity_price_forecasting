f171b87965227c0304224a130edd931216d07a5e
Conrad <49191555+conraaad@users.noreply.github.com>
2025-06-18 01:22:03 +0200

    endpoint /features added

--- service/core/urls.py
+++ service/core/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('health', views.health_check, name='health'),
     path('register', views.RegisterView.as_view(), name='register'),
     path('predict', views.PredictView.as_view(), name='predict'),
+    path('features', views.FeaturesView.as_view(), name='features'),  # Add this line
 ]
